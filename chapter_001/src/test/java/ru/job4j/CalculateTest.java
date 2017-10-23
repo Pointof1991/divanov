81fe9322d7ca507652f6eb21d289b9f83db171e9
Pointof1991 <fatbowl29@yandex.ru>
2017-10-23 15:53:13 +0400

    1.4. JUnit [#4122]

---
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -0,0 +1,29 @@
+package ru.job4j;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Daniil Ivanov (mailto:fatbowl29@yandex.ru)
+* @version 2
+* @since 19.10.2017
+*/
+
+public class CalculateTest {
+ /**
+* Test echo.
+*/ @Test
+
+public void whenTakeNameThenTreeEchoPlusName() {
+    String input = "Daniil Ivanov";
+    String expect = "Echo, echo, echo : Daniil Ivanov"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+}
---
+++ chapter_001/src/test/java/ru/job4j/Package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for testing.
+*
+* @author Daniil Ivanov (mailto:fatbowl29@yandex.ru)
+* @version $2$
+* @since 19.10.2017
+*/
